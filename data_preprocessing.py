897955f735ef141bbe423445acf8077c9d81a3f9
nick kim <nicolas_kim@brown.edu>
2023-11-26 20:48:58 -0500

    fix: clearing pycache

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -72,36 +72,3 @@ def set_splits(clipped_average_xy, splits:list):
 
 training, testing, validation = set_splits(clipped_average_xy, [0.7,0.2,0.1])
 print(np.shape(training), np.shape(testing), np.shape(validation))
-
-# ------------ PLOT PROCESSED DISTRIBUTIONS ----------------- # 
-# NOTE: i should probably get rid of the header in the file
-
-# setr1_counts = np.array(xy[:,2], dtype='int')
-# setr2_counts = np.array(xy[:,3], dtype='int')
-# setr3_counts = np.array(xy[:,4], dtype='int')
-
-# # now plot the above counts => distribution of read counts for all conditions 
-# f, a = plt.subplots(1, 3, figsize=(10,7.5), layout='constrained')
-# f.suptitle('Read Count Distributions')
-# print('-------------- setr1 summary stats ------------')
-# print('max: ', np.max(setr1_counts), 'min: ', np.min(setr1_counts), 'mean: ', np.mean(setr1_counts), 'median', np.median(setr1_counts))
-# a[0].set_xlabel('Frequency')
-# a[0].set_ylabel('Read Counts')
-# a[0].set_title('Setr1 Read Counts')
-# a[0].hist(setr1_counts, bins=np.linspace(1,100))
-
-# print('-------------- setr2 summary stats ------------')
-# print('max: ', np.max(setr2_counts), 'min: ', np.min(setr2_counts), 'mean: ', np.mean(setr2_counts), 'median', np.median(setr2_counts))
-# a[1].set_xlabel('Frequency')
-# a[1].set_ylabel('Read Counts')
-# a[1].set_title('Setr2 Read Counts')
-# a[1].hist(setr2_counts, bins=np.linspace(1,100))
-
-# print('-------------- setr3 summary stats ------------')
-# print('max: ', np.max(setr3_counts), 'min: ', np.min(setr3_counts), 'mean: ', np.mean(setr3_counts), 'median', np.median(setr3_counts))
-# a[2].set_xlabel('Frequency')
-# a[2].set_ylabel('Read Counts')
-# a[2].set_title('Setr3 Read Counts')
-# a[2].hist(setr3_counts, bins=np.linspace(1,100))
-# # plt.show()
-
